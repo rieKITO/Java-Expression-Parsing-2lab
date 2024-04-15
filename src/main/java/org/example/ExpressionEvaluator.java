d7789a5a0540e4b6eaa24e7bfb8ae738fdc169b2
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-15 23:55:34 +0300

    constructor is implemented

--- src/main/java/org/example/ExpressionEvaluator.java
+++ src/main/java/org/example/ExpressionEvaluator.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.Map;
+
 public class ExpressionEvaluator {
+    private Map<String, Double> variables;
+
+    public ExpressionEvaluator(Map<String, Double> variables) {
+        this.variables = variables;
+    }
 }
