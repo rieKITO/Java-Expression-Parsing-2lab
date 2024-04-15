2c19d445d395607c1c1984b9b2f6718ee84601b0
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 00:37:38 +0300

    main fun is implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an expression: ");
+        String expression = scanner.nextLine();
+
+        ExpressionParser parser = new ExpressionParser(expression);
+        List<String> tokens = parser.parse();
+
+        VariableResolver resolver = new VariableResolver();
+        Map<String, Double> variables = resolver.resolveVariables(tokens);
+
+        ExpressionEvaluator evaluator = new ExpressionEvaluator(variables);
+        try {
+            double result = evaluator.evaluate(tokens);
+            System.out.println("Result: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
