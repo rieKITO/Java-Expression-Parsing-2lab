7e3e721ba2db7647ec69f321a2e001a0c80658fd
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-15 23:29:41 +0300

    parsing method is implemented

--- src/main/java/org/example/ExpressionParser.java
+++ src/main/java/org/example/ExpressionParser.java
@@ -1,9 +1,41 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpressionParser {
     private final String expression;
 
     public ExpressionParser(String expression) {
         this.expression = expression;
+    }
+
+    public List<String> parse() {
+        List<String> tokens = new ArrayList<>();
+        StringBuilder token = new StringBuilder();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (!Character.isWhitespace(c)) {
+                if (Character.isDigit(c) || c == '.')
+                    token.append(c);
+                else if (isOperator(c) || c == '(' || c == ')') {
+                    if (!token.isEmpty()) {
+                        tokens.add(token.toString());
+                        token.setLength(0);
+                    }
+                    tokens.add(String.valueOf(c));
+                } else throw new IllegalArgumentException("Incorrect symbol in the expression: " + c);
+            }
+        }
+
+        if (!token.isEmpty())
+            tokens.add(token.toString());
+
+        return tokens;
+    }
+
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
     }
 }
