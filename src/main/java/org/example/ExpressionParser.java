8db7fc52dc8c92a0c0218df110c99ce7fce2b6d7
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-15 23:11:39 +0300

    ExpressionParser constructor implemented

--- src/main/java/org/example/ExpressionParser.java
+++ src/main/java/org/example/ExpressionParser.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class ExpressionParser {
+    private final String expression;
+
+    public ExpressionParser(String expression) {
+        this.expression = expression;
+    }
 }
