044e26984d34425976997c1d9badfb5e063baf8e
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-15 23:50:03 +0300

    resolveVariable method is implemented

--- src/main/java/org/example/VariableResolver.java
+++ src/main/java/org/example/VariableResolver.java
@@ -1,11 +1,32 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class VariableResolver {
-    private Scanner scanner;
+    private final Scanner scanner;
 
     public VariableResolver() {
         this.scanner = new Scanner(System.in);
+    }
+
+    public Map<String, Double> resolveVariables(List<String> tokens) {
+        Map<String, Double> variables = new HashMap<>();
+
+        for (String token : tokens) {
+            if (isVariable(token) && !variables.containsKey(token)) {
+                System.out.print("Enter a value for the " + token + ": ");
+                double value = scanner.nextDouble();
+                variables.put(token, value);
+            }
+        }
+
+        return variables;
+    }
+
+    private boolean isVariable(String token) {
+        return token.matches("[a-zA-Z]+");
     }
 }
