4e080c0a3da58de09245e23e02f4e672797eef7d
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 17:26:46 +0300

    added simple expression test

--- tests/ExpressionEvaluatorTest.java
+++ tests/ExpressionEvaluatorTest.java
@@ -1,2 +1,21 @@
+import org.example.ExpressionEvaluator;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ExpressionEvaluatorTest {
+
+    @Test
+    public void testSimpleExpression() {
+        Map<String, Double> variables = new HashMap<>();
+        variables.put("x", 5.0);
+        ExpressionEvaluator evaluator = new ExpressionEvaluator(variables);
+
+        double result = evaluator.evaluate(Arrays.asList("2", "*", "(", "3", "+", "x", ")"));
+        assertEquals(16.0, result);
+    }
 }
