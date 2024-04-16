eab3707f98da033a09b93cd3b21e7532759d1d8b
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 17:31:28 +0300

    added expression with variables test

--- tests/ExpressionEvaluatorTest.java
+++ tests/ExpressionEvaluatorTest.java
@@ -1,4 +1,5 @@
 import org.example.ExpressionEvaluator;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -9,13 +10,30 @@ import static junit.framework.TestCase.assertEquals;
 
 public class ExpressionEvaluatorTest {
 
+    private Map<String, Double> variables;
+
+    @Before
+    public void setUp() {
+        Map<String, Double> variables = new HashMap<>();
+    }
+
     @Test
     public void testSimpleExpression() {
-        Map<String, Double> variables = new HashMap<>();
         variables.put("x", 5.0);
         ExpressionEvaluator evaluator = new ExpressionEvaluator(variables);
 
         double result = evaluator.evaluate(Arrays.asList("2", "*", "(", "3", "+", "x", ")"));
         assertEquals(16.0, result);
+    }
+
+    @Test
+    public void testExpressionWithVariables() {
+        Map<String, Double> variables = new HashMap<>();
+        variables.put("x", 5.0);
+        variables.put("y", 2.0);
+        ExpressionEvaluator evaluator = new ExpressionEvaluator(variables);
+
+        double result = evaluator.evaluate(Arrays.asList("x", "*", "y"));
+        assertEquals(10.0, result);
     }
 }
