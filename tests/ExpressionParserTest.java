56f2881476893aa26629b3be69be346dfce7ec7e
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 17:40:42 +0300

    added parse simple expression test

--- tests/ExpressionParserTest.java
+++ tests/ExpressionParserTest.java
@@ -1,2 +1,28 @@
+import org.example.ExpressionEvaluator;
+import org.example.ExpressionParser;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ExpressionParserTest {
+
+    @Test
+    public void testParseSimpleExpression() {
+        String expression = "2 * (3 + x)";
+        ExpressionParser parser = new ExpressionParser(expression);
+
+        List<String> tokens = parser.parse();
+
+        assertEquals(7, tokens.size());
+        assertEquals("2", tokens.get(0));
+        assertEquals("*", tokens.get(1));
+        assertEquals("(", tokens.get(2));
+        assertEquals("3", tokens.get(3));
+        assertEquals("+", tokens.get(4));
+        assertEquals("x", tokens.get(5));
+        assertEquals(")", tokens.get(6));
+    }
 }
